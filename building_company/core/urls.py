2d4e7b9a8575148d83299adfc7f982b08fd50386
Roman Drozdov <romandrozdov@icloud.com>
2023-04-03 23:05:27 +0500

    Create template and first SQL quieries.

--- building_company/core/urls.py
+++ building_company/core/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('entity/', views.entity, name='entity'),
+    path('equipment_type/', views.equipment_type, name='equipment_type')
 ]
--- building_company/core/views.py
+++ building_company/core/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.db import connection
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def entity(request):
+    return render(request, "entity.html")
+
+
+def equipment_type(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM equipment_type')
+            rows = cursor.fetchall()
+    finally:
+        cursor.close()
+    print(rows)
+    return render(request, "entity.html")
